029ed2363c9dd2a7f173565447ada0300ae80bbb
Anjali Gupta <anjugupta2468@gmail.com>
2021-04-21 00:17:38 +0530

    class time assignments on Exception Handling

---
+++ Exception Handling/src/com/cust/exception/InvalidInputException.java
@@ -0,0 +1,10 @@
+package com.cust.exception;
+
+public class InvalidInputException extends Exception {
+	public InvalidInputException() {
+	}
+
+	public InvalidInputException(String s) {
+		super(s);
+	}
+}
